3a9694d10cc7732a74c3026f918cf9c2da81787a
urpsychopath <urieslife@gmail.com>
2024-03-18 23:13:30 +0000

    nouvelle interfaces

--- Wahs/urls.py
+++ Wahs/urls.py
@@ -1,8 +1,8 @@
 """
-URL configuration for Wahs project.
+URL configuration for ProjetWah project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,28 +15,41 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from .views import base
-from .views import cv
-from .views import connexion
-from .views import appel_offres
-from .views import ajouter_offres
-from .views import postuler
-from .views import ajouter_personnel
-from .views import ajouter_utilisateur
-
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('base/', base),
-    path('connexion/', connexion, name='connexion'),
-    path('cv/', cv, name='cv'),
-    path('appel_offres/', appel_offres, name='appel_offres'),
-    path('ajouter_offres/', ajouter_offres, name='ajouter_offres'),
-    path('postuler/', postuler, name='postuler'),
-    path('ajouter_personnel/', ajouter_personnel, name='ajouter_personnel'),
-    path('ajouter_utilisateur/', ajouter_utilisateur, name='ajouter_utilisateur'),
-
-    path('backend/', include('backend.urls')),
-
+    path('base/', views.base, name='base'),
+    path('', views.connexion, name='connexion'),
+    path('cv/', views.cv, name='cv'),
+    path('appel_offres/', views.appel_offres, name='appel_offres'),
+    path('ajouter_offres/', views.ajouter_offres, name='ajouter_offres'),
+    path('postuler/', views.postuler, name='postuler'),
+    path('ajouter_personnel/', views.ajouter_personnel, name='ajouter_personnel'),
+    path('ajouter_utilisateur/', views.ajouter_utilisateur, name='ajouter_utilisateur'),
+    path('pays/', views.pays, name='pays'),
+    path('entreprise/', views.entreprise, name='entreprise'),
+    path('experiences/', views.experiences, name='experiences'),
+    path('genre/', views.genre, name='genre'),
+    path('diplome/', views.diplome, name='diplome'),
+    path('cursus/', views.cursus, name='cursus'),
+    path('Type_contrat/', views.Type_contrat, name='Type_contrat'),
+    path('specialite/', views.specialite, name='specialite'),
+    path('profession/', views.profession, name='profession'),
+    path('poste/', views.poste, name='poste'),
+    path('client/', views.client, name='client'),
+    path('compte_comptable/', views.compte_comptable, name='compte_comptable'),
+    path('avoir_specialité/', views.avoir_specialite, name='avoir_specialite'),
+    path('sit_matri/', views.sit_matri, name='sit_matri'),
+    path('type_question/', views.type_question, name='type_question'),
+    path('abonnement/', views.abonnement, name='abonnement'),
+    path('avoir_sit_matri/', views.avoir_sit_matri, name='avoir_sit_matri'),
+    path('avoir_specialite/', views.avoir_specialite, name='avoir_specialite'),
+    path('avoir/', views.avoir, name='avoir'),
+    path('con_traitement/',views.con_traitement, name='con_traitement'),
+    path('faire_entretient/', views.faire_entretient, name='faire_entretient'),
+    path('grp_utilisateur/', views.grp_utilisateur, name='grp_utilisateur'),
+    path('obt_diplome/', views.obt_diplome, name='obt_diplome'),
+    path('type_utilisateur/', views.grp_utilisateur, name='type_utilisateur'),
 ]
+
--- Wahs/views.py
+++ Wahs/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest
 
 def base(request):
+
     return render(request, 'base.html')
 
 def connexion(request):
     return render(request, 'connexion.html')
 
-def cv(request):
+def cv(request : HttpResponse) -> HttpResponse :
     return render(request, 'cv.html')
 
 def appel_offres(request):
@@ -23,3 +25,81 @@ def ajouter_personnel(request):
 
 def ajouter_utilisateur(request):
     return render(request, 'ajouter_utilisateur.html')
+
+def pays(request):
+    return render(request, 'pays.html')
+
+def entreprise(request):
+    return render(request, 'entreprise.html')
+
+def experiences(request):
+    return render(request, 'experiences.html')
+
+def genre(request):
+    return render(request, 'genre.html')
+
+def diplome(request):
+    return render(request, 'diplome.html')
+
+
+def cursus(request):
+    return render(request, 'cursus.html')
+
+def Type_contrat(request):
+    return render(request, 'Type_contrat.html')
+
+def specialite(request):
+    return render(request, 'specialite.html')
+
+def profession(request):
+    return render(request, 'profession.html')
+
+def poste(request):
+    return render(request, 'poste.html')
+
+def client(request):
+    return render(request, 'client.html')
+
+
+def compte_comptable(request):
+    return render(request, 'compte_comptable.html')
+
+def avoir_specialite(request):
+    return render(request, 'avoir_specialite.html')
+
+def employe(request):
+    return render(request, 'employe.html')
+
+def sit_matri(request):
+    return render(request, 'sit_matri.html')
+
+def type_question(request):
+    return render(request, 'type_question.html')
+
+
+
+
+
+def abonnement(request):
+    return render(request, 'abonnement.html')
+
+def avoir_sit_matri(request):
+    return render(request, 'avoir_sit_matri.html')
+
+def avoir_specialite(request):
+    return render(request, 'avoir_specialite.html')
+
+def avoir(request):
+    return render(request, 'avoir.html')
+
+def con_traitement(request):
+    return render(request, 'con_traitement.html')
+
+def faire_entretient(request):
+    return render(request, 'faire_entretient.html')
+
+def grp_utilisateur(request):
+    return render(request, 'grp_utilisateur.html')
+
+def obt_diplome(request):
+    return render(request, 'obt_diplome.html')
